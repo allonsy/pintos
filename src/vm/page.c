6aaee98925cc5fec2f252f915342d571aa3026b9
acy1 <alejandro.younger@gmail.com>
2015-11-13 15:07:10 -0600

    start frame.c functions

---
+++ src/vm/page.c
@@ -0,0 +1,81 @@
+#include "page.h"
+
+#define STACK_MAX (1024 * 1024)
+
+
+static void 
+destroy_page (struct hash_elem *p_, void *aux UNUSED)  
+{
+	return;
+}
+
+static struct 
+page *page_for_addr (const void *address) 
+{
+	return NULL;
+}
+
+static bool do_page_in (struct page *p) 
+{
+	return false;
+}
+
+void 
+page_exit (void)  
+{
+	return;
+}
+
+bool 
+page_in (void *fault_addr) 
+{
+	return false;
+}
+
+bool 
+page_out (struct page *p) 
+{
+	return false;
+}
+
+bool 
+page_accessed_recently (struct page *p) 
+{
+	return false;
+}
+
+struct page * 
+page_allocate (void *vaddr, bool read_only) 
+{
+	return false;
+}
+
+void 
+page_deallocate (void *vaddr) 
+{
+	return false;
+}
+
+unsigned 
+page_hash (const struct hash_elem *e, void *aux UNUSED) 
+{
+	return 0;
+}
+
+bool 
+page_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED) 
+{
+	return false;
+}
+
+bool 
+page_lock (const void *addr, bool will_write) 
+{
+	return false;
+}
+
+void 
+page_unlock (const void *addr) 
+{
+	return;
+}
