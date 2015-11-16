463d5805fa60c68e31320823cbabfbe8bb6198a2
Alejandro <acy1@uchicago.edu>
2015-11-15 22:47:31 -0600

    send minor changes to mac

--- pintos-p3/src/vm/frame.h
+++ pintos-p3/src/vm/frame.h
@@ -17,10 +17,10 @@ struct frame
 
 
 void frame_init(void);
-//struct frame* try_frame_alloc_and_lock (struct page *page);
-struct frame* try_frame_alloc_and_lock (void);
-void frame_lock (struct page *p);
+struct frame* try_frame_alloc_and_lock (struct page *page);
+//struct frame* try_frame_alloc_and_lock (void);
+void frame_lock (struct page *page);
 void frame_free (struct frame *f);
-void frame_unlock (struct frame *f);
+void frame_unlock (struct page *page);
 
 #endif /* VM_FRAME_H */
