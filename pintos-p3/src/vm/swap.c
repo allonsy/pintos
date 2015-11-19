d223e9c5c8d0ba672e013b391755e3a2653b5bdc
acy1 <alejandro.younger@gmail.com>
2015-11-19 00:59:59 -0600

    send swap.c to CSIL for testing

--- pintos-p3/src/vm/swap.c
+++ pintos-p3/src/vm/swap.c
@@ -1,6 +1,7 @@
 #include "swap.h"
 #include "page.h"
 #include "threads/vaddr.h"
+#include "block.h"
 
 
 /*
@@ -34,6 +35,9 @@ static struct bitmap *swap_bitmap;
 /* Protects swap_bitmap. */
 static struct lock swap_lock;
 
+/* index in the bitmap of the first free bit */
+static size_t idx_first_free;
+
 /* Number of sectors per page. */
 #define PAGE_SECTORS (PGSIZE / BLOCK_SECTOR_SIZE)
 
@@ -52,11 +56,14 @@ swap_init (void)
                                  / PAGE_SECTORS);
   if (swap_bitmap == NULL)
     PANIC ("couldn't create swap bitmap");
+
+  idx_first_free = 0;
   lock_init (&swap_lock);
 }
 
-/* assumes that the page is held in a valid block sector */
-
+/* assumes that the page is held in a valid block sector 
+  and that frame f is available, the hard bits are handled
+  elsewhere. This function just writes into the frame */
 bool
 swap_in (struct page *p, struct frame *f)
 {
@@ -65,13 +72,25 @@ swap_in (struct page *p, struct frame *f)
   // - block_read()
   // - bitmap_reset()
 
-  if(!lock_held_by_current_thread(&swap_lock))
+  int i;
+
+  /* this lock on the outside since it has more contexts
+    in which it can be locked */
+  frame_lock(f);
+
+  for(i = 0; i < PAGE_SECTORS; i++)
   {
-    lock_acquire(&swap_lock);
+    block_read (swap_device, p->sector + i, f->base + (BLOCK_SECTOR_SIZE * i));
   }
 
+  size_t bit_idx = p->sector / PAGE_SECTORS;
 
+  lock_acquire(&swap_lock);
+  bitmap_reset (swap_bitmap, bit_idx);
+  idx_first_free = bit_idx < idx_first_free ? bit_idx : idx_first_free;
   lock_release(&swap_lock);
+
+  frame_unlock(f);
 	// vestigial
   return false;
 }
@@ -79,19 +98,44 @@ swap_in (struct page *p, struct frame *f)
 bool 
 swap_out (struct page *p) 
 {
-  
-  // might want to use these functions:
-  // - lock_held_by_current_thread()
-  // - bitmap_scan_and_flip()
-  // - block_write()
 
-  if(!lock_held_by_current_thread(&swap_lock))
+  size_t bit_idx;
+
+  /* this lock on the outside since it has more contexts
+    in which it can be locked */
+  frame_lock(&p->frame);
+  lock_acquire(&swap_lock);
+
+  // make sure idx_first_free is in fact free
+  if(!bitmap_test (swap_bitmap, idx_first_free))
   {
-    lock_acquire(&swap_lock);
+    bit_idx = idx_first_free;
+    bitmap_mark (swap_bitmap, bit_idx);
+    idx_first_free = bitmap_scan (swap_bitmap, idx_first_free+1, 1, false);
   }
-    
-
+  else
+  {
+    // theoretically idx_first_free is handled correctly and this never happens
+    // but otherwise we spend time scanning the bitmap to make sure it 
+    // gets reset correctly
+    bit_idx = bitmap_scan (swap_bitmap, 0, 1, false);
+    bitmap_mark (swap_bitmap, bit_idx);
+    idx_first_free = bitmap_scan (swap_bitmap, 0, 1, false);
+  }
+  block_sector_t start = PAGE_SECTORS * bit_idx;
   lock_release(&swap_lock);
+
+  for(i = 0; i < PAGE_SECTORS; i++)
+  {
+    /* DEFINITELY DOUBLE CHECK MY ARITHMETIC!! */
+    block_write (swap_device, start + i, p->frame->base + (i * BLOCK_SECTOR_SIZE));
+  }
+
+  p->sector = start;
+
+  frame_unlock(&p->frame);
+
+  frame_free(&p->frame);
   //vestigial
   return false;
 }
